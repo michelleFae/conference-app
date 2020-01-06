a217e5351541d1e413a4f99149f5879dee20c472
Michelle D Souza <michelled@berkeley.edu>
2020-01-06 16:58:58 +0530

    initial commit
    Experimenting with Open Source Lightning Web Components
    
    link to spec: https://trailhead.salesforce.com/content/learn/projects/build-your-first-app-with-lightning-web-components-open-source

---
+++ src/client/modules/data/sessionService/sessionService.js
@@ -0,0 +1,20 @@
+//NOTE: I CHANGED THE FILE PATH OF THE DATA FOLDER(ORIGINIALLY INSIDE MY) in order to fix bug in spec
+const URL = 'https://conference-lwc-app.herokuapp.com/api/sessions';
+let sessions = [];
+export const getSessions = () =>
+    fetch(URL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No response from server');
+            }
+            return response.json();
+        })
+        .then(result => {
+            sessions = result.data;
+            return sessions;
+        });
+export const getSession = sessionId => {
+    return sessions.find(session => {
+        return session.id === sessionId;
+    });
+};
